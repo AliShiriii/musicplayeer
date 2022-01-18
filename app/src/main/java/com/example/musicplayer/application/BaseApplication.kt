dfa7b0217e69b1835449df8862eb6348bcd424bd
Ali__Shiri <alishiri067@gmail.com>
2022-01-18 11:03:07 -0800

    add base Application

---
+++ app/src/main/java/com/example/musicplayer/application/BaseApplication.kt
@@ -0,0 +1,17 @@
+package com.example.musicplayer.application
+
+import android.app.Application
+import android.content.Context
+import androidx.multidex.MultiDex
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BaseApplication: Application() {
+
+    //BaseApplication
+    override fun attachBaseContext(base: Context) {
+        super.attachBaseContext(base)
+        MultiDex.install(this)
+
+    }
+}
