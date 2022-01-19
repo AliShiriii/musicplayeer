d021a622e6900ddacd9feacf5bb7da96e2524dff
Ali__Shiri <alishiri067@gmail.com>
2022-01-19 07:13:57 -0800

    add playFragment

--- app/src/main/java/com/example/musicplayer/view/PlayerFragment.kt
+++ app/src/main/java/com/example/musicplayer/view/PlayerFragment.kt
@@ -1,20 +1,36 @@
 package com.example.musicplayer.view
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.musicplayer.R
+import androidx.fragment.app.Fragment
+import com.example.musicplayer.databinding.FragmentPlayerBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class PlayerFragment : Fragment() {
+
+    private var _binding: FragmentPlayerBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_player, container, false)
+
+        _binding = FragmentPlayerBinding.inflate(inflater, container, false)
+
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+
     }
 
 }
